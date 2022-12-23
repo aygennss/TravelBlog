77cc33eededc7fcb7c5acc6908254e7815f211c2
aygennss <dnzaygen@gmail.com>
2022-12-23 21:11:29 +0300

    Asp.net

---
+++ TravelTrip/Models/Siniflar/Afmin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTrip.Models.Siniflar
+{
+    public class Afmin
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Kullanici { get; set; }
+        public string sifre { get; set; }
+    }
+}
---
+++ TravelTrip/Models/Siniflar/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TravelTrip.Models.Siniflar
+{
+    public class Context : DbContext
+    {
+        public DbSet<Afmin> Admins { get; set; }
+        public DbSet<AdresBlog> AdresBlogs { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Hakkimizda> Hakkimizdas { get; set; }
+        public DbSet<İletisim > iletisims { get; set; }
+        public DbSet<Yorumlar > Yorumlars { get; set; }
+
+    }
+}
